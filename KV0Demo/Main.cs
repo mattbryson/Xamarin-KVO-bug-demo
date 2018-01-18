9793357f3d593b9be06913ff2e2157c0f2d4672e
Matt Bryson <matt.bryson@gmail.com>
2018-01-18 14:45:00 +0000

    first commit

---
+++ KV0Demo/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace KV0Demo
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
---
+++ KV0Demo/ViewController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace KV0Demo
+{
+    public partial class ViewController : NSViewController
+    {
+
+        private Boolean _checkValue;
+        [Export("CheckValue")]
+        public Boolean CheckValue
+        {
+            get { return _checkValue; }
+            set { WillChangeValue("CheckValue"); _checkValue = value; DidChangeValue("CheckValue"); }
+        }
+
+
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            this.AddObserver(this, (NSString)"CheckValue", NSKeyValueObservingOptions.New, this.Handle);
+
+            //Programtically update the value
+            CheckValue = false;
+        }
+
+
+
+        public override void ObserveValue(NSString keyPath, NSObject ofObject, NSDictionary change, IntPtr context)
+        {
+            var msg = string.Format("Observer triggered for {0}", keyPath);
+            Console.WriteLine( msg );
+
+            LogField.StringValue = LogField.StringValue + "\n" +  msg;
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
